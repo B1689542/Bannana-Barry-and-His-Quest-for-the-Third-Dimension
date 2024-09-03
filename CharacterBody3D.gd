90c1907da15a5db2ce018796a27f1679dca2da51
B1689542 <156558600+B1689542@users.noreply.github.com>
2024-09-03 12:29:33 -0400

    Update CharacterBody3D.gd

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -4,30 +4,30 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var speed = 4.0  # movement speed
 var jump_speed = 6.0  # determines jump height
 var mouse_sensitivity = 0.002  # turning speed
-var moveturnhandling = [["strafe_left", "strafe_right", "move_forward", "move_back"],[-90,90,0,180]]
-var turn = 0 
-var turning = 0.00
-var camerapoint = 0.00
+#var moveturnhandling = [["strafe_left", "strafe_right", "move_forward", "move_back"],[-90,90,0,180]]
+#var turn = 0 
+#var turning = 0.00
+#var camerapoint = 0.00
 
 func get_input():
 	var input = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
 	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
 	velocity.x = movement_dir.x * speed
 	velocity.z = movement_dir.z * speed
-	$MeshInstance3D.look_at(transform.origin + velocity, Vector3(0, 1, 0))
 
 func turnfunc():
 	pass
 
 func _physics_process(delta):
-	velocity.y += -gravity * delta
 	get_input()
+	$MeshInstance3D.look_at(transform.origin + velocity, Vector3(0, 1, 0))
+	velocity.y += -gravity * delta
 	move_and_slide()
 	turnfunc()
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		camerapoint += (-event.relative.x * mouse_sensitivity)
+		#camerapoint += (-event.relative.x * mouse_sensitivity)
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$MeshInstance3D.rotate_y(-(-event.relative.x * mouse_sensitivity))
 		$CollisionShape3D.rotate_y(-(-event.relative.x * mouse_sensitivity))
