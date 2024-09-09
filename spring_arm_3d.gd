6e6914ddd7a67eaba71eea98b56b782dc2889a5e
B1689542 <156558600+B1689542@users.noreply.github.com>
2024-09-09 09:15:00 -0400

    kngly

--- spring_arm_3d.gd
+++ spring_arm_3d.gd
@@ -1,9 +1,10 @@
 extends SpringArm3D
 
-
+var test = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#rotate_y(45)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,6 +14,7 @@ func _process(delta: float) -> void:
 func _input(event):
 	if event is InputEventMouseMotion:
 		print("Mouse Motion at: ", event.position)
-		rotate_y(.005 * -event.relative.x)
-		#rotate_x(.001 * event.relative.y)
-		#rotate_z(.005 * event.relative.y) #havin a spot of bother with this one
+		#rotate_y(.005 * -event.relative.x)
+		test = event.relative.y
+		rotate_x(.005 * test * .5)
+		rotate_z(.005 * test * -.5) #havin a spot of bother with this one
