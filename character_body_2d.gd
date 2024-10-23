b13b71dcd81b93a404d2da4e16750003604a57a0
B1689542 <156558600+B1689542@users.noreply.github.com>
2024-10-23 19:44:16 -0400

    added basic player movement
    Added player sprite, as wel as player collision, and basic walking and jumping functions, which will be tweaked later in development

---
+++ character_body_2d.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -600.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta * 2
+		$CollisionShape2D/AnimatedSprite2D.stop()
+	
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if is_on_floor():
+			$CollisionShape2D/AnimatedSprite2D.play("walk")
+		$CollisionShape2D/AnimatedSprite2D.scale.x = direction
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$CollisionShape2D/AnimatedSprite2D.stop()
+
+	move_and_slide()
