60228b44a43257fa1e8bc7c4219fa581c7e2c56a
B1689542 <156558600+B1689542@users.noreply.github.com>
2024-10-26 14:20:21 -0400

    added all four boundaries
    added celing and right wall boundaries, as well as the door collision and marker for when the player enters the room, and scaled the floor to accurate size

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -8,7 +8,7 @@ const JUMP_VELOCITY = -1000.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 2
+		velocity += get_gravity() * delta * 3
 		$CollisionShape2D/AnimatedSprite2D.stop()
 	
 
