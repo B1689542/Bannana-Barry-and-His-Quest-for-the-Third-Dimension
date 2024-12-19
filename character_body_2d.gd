37ee8a4b31cd78e3342a3b26e1d5b67838f91b9f
B1689542 <156558600+B1689542@users.noreply.github.com>
2024-12-19 14:04:10 -0500

    Added a texture
    Added a basic background texture for the stage, mostly placeholder art for the moment

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,20 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -1000.0
-
+const SPEED = 600.0
+const JUMP_VELOCITY = -1500.0
+var jumps_left = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 3
 		$CollisionShape2D/AnimatedSprite2D.stop()
-	
+	if is_on_floor():
+		jumps_left = 1
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif jumps_left == 1:
+			velocity.y = JUMP_VELOCITY
+			jumps_left = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
